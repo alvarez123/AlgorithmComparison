c1d37904c0d9658bdc64e09b72263ccbf3e04c5e
Anil Sevici <anilsevici@hotmail.com>
2015-05-16 16:23:28 +0300

    Initial Commit

---
+++ src/main/java/AlgoritmaAnalizi/AlgoritmaAnalizi/App.java
@@ -0,0 +1,13 @@
+package AlgoritmaAnalizi.AlgoritmaAnalizi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
